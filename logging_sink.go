ee0de5c775bc77193aaa17128fe23f052520df5b
Rich Unger <rich.unger@gmail.com>
2017-05-24 15:24:41 -0700

    Don't flood devel logs (#12)

--- logging_sink.go
+++ logging_sink.go
@@ -10,13 +10,13 @@ func NewLoggingSink() Sink {
 }
 
 func (s *loggingSink) FlushCounter(name string, value uint64) {
-	logger.Infof("[gostats] flushing counter %s: %d", name, value)
+	logger.Debugf("[gostats] flushing counter %s: %d", name, value)
 }
 
 func (s *loggingSink) FlushGauge(name string, value uint64) {
-	logger.Infof("[gostats] flushing gauge %s: %d", name, value)
+	logger.Debugf("[gostats] flushing gauge %s: %d", name, value)
 }
 
 func (s *loggingSink) FlushTimer(name string, value float64) {
-	logger.Infof("[gostats] flushing time %s: %f", name, value)
+	logger.Debugf("[gostats] flushing time %s: %f", name, value)
 }
