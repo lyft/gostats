c2149fa991932125081b5c45f0b906669078a191
Chris Roche <croche@lyft.com>
2017-10-01 13:35:17 -0700

    Expose all optional http.ResponseWriter interfaces

--- stat_handler.go
+++ stat_handler.go
@@ -6,6 +6,8 @@ import (
 	"sync"
 )
 
+const requestTimer = "rq_time_us"
+
 type httpHandler struct {
 	prefix   string
 	scope    Scope
@@ -22,7 +24,7 @@ func NewStatHandler(scope Scope, handler http.Handler) http.Handler {
 	return &httpHandler{
 		scope:    scope,
 		delegate: handler,
-		timer:    scope.NewTimer("rq_time_us"),
+		timer:    scope.NewTimer(requestTimer),
 		codes:    map[int]Counter{},
 	}
 }
@@ -49,7 +51,6 @@ func (h *httpHandler) counter(code int) Counter {
 func (h *httpHandler) wrapResponse(w http.ResponseWriter) http.ResponseWriter {
 	rw := &responseWriter{
 		ResponseWriter: w,
-		span:           h.timer.AllocateSpan(),
 		handler:        h,
 	}
 
@@ -59,31 +60,106 @@ func (h *httpHandler) wrapResponse(w http.ResponseWriter) http.ResponseWriter {
 	closeNotifier, canNotify := w.(http.CloseNotifier)
 
 	if canFlush && canHijack && canPush && canNotify {
-		return struct{
+		return struct {
 			http.ResponseWriter
 			http.Flusher
 			http.Hijacker
 			http.Pusher
 			http.CloseNotifier
-		}{ rw, flusher, hijacker, pusher, closeNotifier}
+		}{rw, flusher, hijacker, pusher, closeNotifier}
 	} else if canFlush && canHijack && canPush {
-		return struct{
+		return struct {
 			http.ResponseWriter
 			http.Flusher
 			http.Hijacker
 			http.Pusher
+		}{rw, flusher, hijacker, pusher}
+	} else if canFlush && canHijack && canNotify {
+		return struct {
+			http.ResponseWriter
+			http.Flusher
+			http.Hijacker
 			http.CloseNotifier
-		}{ rw, flusher, hijacker, pusher, closeNotifier}
+		}{rw, flusher, hijacker, closeNotifier}
+	} else if canFlush && canPush && canNotify {
+		return struct {
+			http.ResponseWriter
+			http.Flusher
+			http.Pusher
+			http.CloseNotifier
+		}{rw, flusher, pusher, closeNotifier}
+	} else if canHijack && canPush && canNotify {
+		return struct {
+			http.ResponseWriter
+			http.Hijacker
+			http.Pusher
+			http.CloseNotifier
+		}{rw, hijacker, pusher, closeNotifier}
+	} else if canFlush && canHijack {
+		return struct {
+			http.ResponseWriter
+			http.Flusher
+			http.Hijacker
+		}{rw, flusher, hijacker}
+	} else if canFlush && canPush {
+		return struct {
+			http.ResponseWriter
+			http.Flusher
+			http.Pusher
+		}{rw, flusher, pusher}
+	} else if canFlush && canNotify {
+		return struct {
+			http.ResponseWriter
+			http.Flusher
+			http.CloseNotifier
+		}{rw, flusher, closeNotifier}
+	} else if canHijack && canPush {
+		return struct {
+			http.ResponseWriter
+			http.Hijacker
+			http.Pusher
+		}{rw, hijacker, pusher}
+	} else if canHijack && canNotify {
+		return struct {
+			http.ResponseWriter
+			http.Hijacker
+			http.CloseNotifier
+		}{rw, hijacker, closeNotifier}
+	} else if canPush && canNotify {
+		return struct {
+			http.ResponseWriter
+			http.Pusher
+			http.CloseNotifier
+		}{rw, pusher, closeNotifier}
+	} else if canFlush {
+		return struct {
+			http.ResponseWriter
+			http.Flusher
+		}{rw, flusher}
+	} else if canHijack {
+		return struct {
+			http.ResponseWriter
+			http.Hijacker
+		}{rw, hijacker}
+	} else if canPush {
+		return struct {
+			http.ResponseWriter
+			http.Pusher
+		}{rw, pusher}
+	} else if canNotify {
+		return struct {
+			http.ResponseWriter
+			http.CloseNotifier
+		}{rw, closeNotifier}
 	}
 
 	return rw
 }
 
 func (h *httpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
-
-	h.delegate.ServeHTTP(rw, r)
-	rw.span.Complete()
+	span := h.timer.AllocateSpan()
+	h.delegate.ServeHTTP(h.wrapResponse(w), r)
+	span.Complete()
 }
 
 type responseWriter struct {
--- stat_handler_test.go
+++ stat_handler_test.go
@@ -1,24 +1,207 @@
 package stats
 
 import (
+	"fmt"
+	"io"
 	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"strings"
+	"sync"
 	"testing"
 )
 
-func TestResponseWriter_InterfacePassthrough(t *testing.T) {
+func TestHttpHandler_ServeHTTP(t *testing.T) {
 	t.Parallel()
 
-	w := struct {
-		http.ResponseWriter
-		http.Flusher
-		http.Hijacker
-		http.Pusher
-		http.CloseNotifier
-	}{}
+	sink := NewMockSink()
+	store := NewStore(sink, false)
 
-	var rw http.ResponseWriter = &responseWriter{ResponseWriter: w}
+	h := NewStatHandler(
+		store,
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if code, err := strconv.Atoi(r.Header.Get("code")); err == nil {
+				w.WriteHeader(code)
+			}
 
-	if _, ok := rw.(http.Flusher); !ok {
-		t.Fatal("stat responseWriter should pass through Flusher")
+			io.Copy(w, r.Body)
+			r.Body.Close()
+		})).(*httpHandler)
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	go func() {
+		r, _ := http.NewRequest(http.MethodGet, "/", strings.NewReader("foo"))
+		w := httptest.NewRecorder()
+		h.ServeHTTP(w, r)
+		store.Flush()
+
+		if w.Body.String() != "foo" {
+			t.Errorf("wanted %q body, got %q", "foo", w.Body.String())
+		}
+
+		if w.Code != http.StatusOK {
+			t.Errorf("wanted 200, got %d", w.Code)
+		}
+
+		wg.Done()
+	}()
+
+	go func() {
+		r := httptest.NewRequest(http.MethodGet, "/", strings.NewReader("bar"))
+		r.Header.Set("code", strconv.Itoa(http.StatusNotFound))
+		w := httptest.NewRecorder()
+		h.ServeHTTP(w, r)
+		store.Flush()
+
+		if w.Body.String() != "bar" {
+			t.Errorf("wanted %q body, got %q", "bar", w.Body.String())
+		}
+
+		if w.Code != http.StatusNotFound {
+			t.Errorf("wanted 404, got %d", w.Code)
+		}
+
+		wg.Done()
+	}()
+
+	wg.Wait()
+
+	timer, ok := sink.Timers[requestTimer]
+	if !ok {
+		t.Errorf("wanted a %q timer, none found", requestTimer)
+	} else if timer != 2 {
+		t.Error("wanted 2, got", timer)
+	}
+
+	c, ok := sink.Counters["200"]
+	if !ok {
+		t.Error("wanted a '200' counter, none found")
+	} else if c != 1 {
+		t.Error("wanted 1, got", c)
+	}
+
+	c, ok = sink.Counters["404"]
+	if !ok {
+		t.Error("wanted a '404' counter, none found")
+	} else if c != 1 {
+		t.Error("wanted 1, got", c)
+	}
+}
+
+func TestHTTPHandler_WrapResponse(t *testing.T) {
+	t.Parallel()
+
+	tests := []http.ResponseWriter{
+		struct {
+			http.ResponseWriter
+			http.Flusher
+			http.Hijacker
+			http.Pusher
+			http.CloseNotifier
+		}{},
+		struct {
+			http.ResponseWriter
+			http.Flusher
+			http.Hijacker
+			http.Pusher
+		}{},
+		struct {
+			http.ResponseWriter
+			http.Flusher
+			http.Hijacker
+			http.CloseNotifier
+		}{},
+		struct {
+			http.ResponseWriter
+			http.Flusher
+			http.Hijacker
+		}{},
+		struct {
+			http.ResponseWriter
+			http.Flusher
+			http.Pusher
+			http.CloseNotifier
+		}{},
+		struct {
+			http.ResponseWriter
+			http.Flusher
+			http.Pusher
+		}{},
+		struct {
+			http.ResponseWriter
+			http.Flusher
+			http.CloseNotifier
+		}{},
+		struct {
+			http.ResponseWriter
+			http.Flusher
+		}{},
+		struct {
+			http.ResponseWriter
+			http.Hijacker
+			http.Pusher
+			http.CloseNotifier
+		}{},
+		struct {
+			http.ResponseWriter
+			http.Hijacker
+			http.Pusher
+		}{},
+		struct {
+			http.ResponseWriter
+			http.Hijacker
+			http.CloseNotifier
+		}{},
+		struct {
+			http.ResponseWriter
+			http.Hijacker
+		}{},
+		struct {
+			http.ResponseWriter
+			http.Pusher
+			http.CloseNotifier
+		}{},
+		struct {
+			http.ResponseWriter
+			http.Pusher
+		}{},
+		struct {
+			http.ResponseWriter
+			http.CloseNotifier
+		}{},
+		struct{ http.ResponseWriter }{},
+	}
+
+	h := NewStatHandler(
+		NewStore(NewNullSink(), false),
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})).(*httpHandler)
+
+	for i, test := range tests {
+		tc := test
+		t.Run(fmt.Sprint("test:", i), func(t *testing.T) {
+			t.Parallel()
+
+			_, canFlush := tc.(http.Flusher)
+			_, canHijack := tc.(http.Hijacker)
+			_, canPush := tc.(http.Pusher)
+			_, canNotify := tc.(http.CloseNotifier)
+
+			rw := h.wrapResponse(tc)
+
+			if _, ok := rw.(http.Flusher); ok != canFlush {
+				t.Errorf("Flusher: wanted %t", canFlush)
+			}
+			if _, ok := rw.(http.Hijacker); ok != canHijack {
+				t.Errorf("Hijacker: wanted %t", canHijack)
+			}
+			if _, ok := rw.(http.Pusher); ok != canPush {
+				t.Errorf("Pusher: wanted %t", canPush)
+			}
+			if _, ok := rw.(http.CloseNotifier); ok != canNotify {
+				t.Errorf("CloseNotifier: wanted %t", canNotify)
+			}
+		})
 	}
 }
