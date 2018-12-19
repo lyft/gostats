f9f04871a982960a70f98e09ccc74bd78243cbf7
Charlie Vieth <charlie.vieth@gmail.com>
2018-12-19 13:59:11 -0500

    Improve the performance of New* methods and tcpStatsdSink (#50)
    * null_sink: use value receiver instead of pointer
    
    struct{} has size 0, so don't allocate a pointer for it.
    
    * Bnechmark serializeTags
    
    goos: darwin
    goarch: amd64
    pkg: github.com/lyft/gostats
    BenchmarkSerializeTags-8   	  500000	      2582 ns/op	     688 B/op	      29 allocs/op
    PASS
    ok  	github.com/lyft/gostats	1.341s
    
    * Improve the performance of serializeTags
    
    Benchmark Result:
    
    goos: darwin
    goarch: amd64
    pkg: github.com/lyft/gostats
    BenchmarkSerializeTags-8   	 3000000	       472 ns/op	     256 B/op	       3 allocs/op
    PASS
    ok  	github.com/lyft/gostats	1.918s
    
    Benchmark Comparison:
    
    benchmark                    old ns/op     new ns/op     delta
    BenchmarkSerializeTags-8     2582          472           -81.72%
    
    benchmark                    old allocs     new allocs     delta
    BenchmarkSerializeTags-8     29             3              -89.66%
    
    benchmark                    old bytes     new bytes     delta
    BenchmarkSerializeTags-8     688           256           -62.79%
    
    * Benchmark NewCounter
    
    This is to make sure we don't have any regresions around serializeTags
    or other shared NewCounter things.
    
    goos: darwin
    goarch: amd64
    pkg: github.com/lyft/gostats
    BenchmarkStore_NewCounterWithTags-8   	 3000000	       569 ns/op	     336 B/op	       4 allocs/op
    PASS
    ok  	github.com/lyft/gostats	2.306s
    
    * Use a global buffer pool for tcp_sink
    
    Buffer pools should be global, not local to each struct.  Under the hood
    sync.Pool uses "thread local" storage so it's best to share.
    
    * Don't use fmt.Sprintf() to concatenate two strings
    
    * Reimplement strings.Builder to support older versions of Go
    
    This is the exact same behavior as strings.Builder minus the copy check,
    which we don't have to worry about and it compatible with Go versions
    1.9 and earlier.

--- tags.go
+++ tags.go
@@ -1,20 +1,17 @@
 package stats
 
 import (
-	"bytes"
-	"fmt"
-	"regexp"
 	"sort"
-)
-
-const (
-	tagPrefix   = ".__"
-	tagSep      = "="
-	tagFailsafe = "_"
+	"strings"
+	"unsafe"
 )
 
 // illegalTagValueChars are loosely set to ensure we don't have statsd parse errors.
-var illegalTagValueChars = regexp.MustCompile(`[:|.]`)
+var illegalTagValueCharsReplacer = strings.NewReplacer(
+	".", "_",
+	":", "_",
+	"|", "_",
+)
 
 type tagPair struct {
 	dimension string
@@ -28,19 +25,30 @@ func (t tagSet) Swap(i, j int)      { t[i], t[j] = t[j], t[i] }
 func (t tagSet) Less(i, j int) bool { return t[i].dimension < t[j].dimension }
 
 func serializeTags(tags map[string]string) string {
+	const prefix = ".__"
+	const sep = "="
+
 	if len(tags) == 0 {
 		return ""
 	}
-	tagPairs := make([]tagPair, 0, len(tags))
-	for tagKey, tagValue := range tags {
-		tagValue = illegalTagValueChars.ReplaceAllLiteralString(tagValue, tagFailsafe)
-		tagPairs = append(tagPairs, tagPair{tagKey, tagValue})
+	pairs := make([]tagPair, 0, len(tags))
+	n := (len(prefix) + len(sep)) * len(tags)
+	for k, v := range tags {
+		n += len(k) + len(v)
+		pairs = append(pairs, tagPair{
+			dimension: k,
+			value:     illegalTagValueCharsReplacer.Replace(v),
+		})
 	}
-	sort.Sort(tagSet(tagPairs))
+	sort.Sort(tagSet(pairs))
 
-	buf := new(bytes.Buffer)
-	for _, tag := range tagPairs {
-		fmt.Fprint(buf, tagPrefix, tag.dimension, tagSep, tag.value)
+	// CEV: this is same as strings.Builder, but works with go1.9 and earlier
+	b := make([]byte, 0, n)
+	for _, tag := range pairs {
+		b = append(b, prefix...)
+		b = append(b, tag.dimension...)
+		b = append(b, sep...)
+		b = append(b, tag.value...)
 	}
-	return buf.String()
+	return *(*string)(unsafe.Pointer(&b))
 }
