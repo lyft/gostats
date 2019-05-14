5eeb0df4ba965c588eb978fa6f8e0f1f01958821
Charlie Vieth <cvieth@lyft.com>
2019-05-13 21:17:01 -0400

    NewDefaultStore: don't export stats by default
    If export is true then stats are exported via the expvars package, which
    gostats uses incorrectly (it should only be used in init() functions)
    and as a result is slow (it resorts all stored variables whenever a new
    one is added).

--- stats.go
+++ stats.go
@@ -194,7 +194,7 @@ func NewDefaultStore() Store {
 		newStore = NewStore(NewLoggingSink(), false)
 		go newStore.Start(time.NewTicker(10 * time.Second))
 	} else {
-		newStore = NewStore(NewTCPStatsdSink(), true)
+		newStore = NewStore(NewTCPStatsdSink(), false)
 		go newStore.Start(time.NewTicker(time.Duration(settings.FlushIntervalS) * time.Second))
 	}
 	return newStore
