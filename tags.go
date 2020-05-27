d95cbc015671ba6dfa360b998633121f816281d5
Charlie Vieth <charlie.vieth@gmail.com>
2020-05-27 17:19:53 -0400

    subScope: use a slice of tags instead of a map (#97)
    * subScope: use a slice of tags instead of a map
    
    This commit changes the subScope to keep a slice of tags instead of a
    map. This means that we no longer modify or "own" the map we're passed,
    which has been a source of bugs recently (that gostats owns any map
    passed to it was poorly, if at all, documented).
    
    This change also improves performance by 2x and reduces memory usage and
    GC pressure as each subScope will no longer be holding a map (which is
    sparse and the GC must scan).
    
    This change also moves us closer to passing tags via a slice (instead of
    a map) which is a change that I intend to make part of the V1 release
    (assuming I ever get around to that).
    
    ```
    benchmark                                old ns/op     new ns/op     delta
    BenchmarkStore_ScopeWithTags-12          1321          646           -51.10%
    BenchmarkStore_ScopeNoTags-12            583           179           -69.30%
    
    benchmark                                old allocs     new allocs     delta
    BenchmarkStore_ScopeWithTags-12          4              3              -25.00%
    BenchmarkStore_ScopeNoTags-12            4              2              -50.00%
    
    benchmark                                old bytes     new bytes     delta
    BenchmarkStore_ScopeWithTags-12          544           512           -5.88%
    BenchmarkStore_ScopeNoTags-12            304           112           -63.16%
    ```
    
    * Update tags.go
    
    Co-authored-by: Max R <mxr@users.noreply.github.com>
    
    * stats_test: code review comments
    
    * tags_test: benchmark tagSet.Search
    
    * tags: rename CompareAndSwap => cas and document network sort
    
    * tags: save alloc in replaceChars
    
    Co-authored-by: Max R <mxr@users.noreply.github.com>

--- tags.go
+++ tags.go
@@ -6,15 +6,209 @@ import (
 )
 
 type tagPair struct {
-	dimension string
-	value     string
+	key   string
+	value string
 }
 
 type tagSet []tagPair
 
 func (t tagSet) Len() int           { return len(t) }
 func (t tagSet) Swap(i, j int)      { t[i], t[j] = t[j], t[i] }
-func (t tagSet) Less(i, j int) bool { return t[i].dimension < t[j].dimension }
+func (t tagSet) Less(i, j int) bool { return t[i].key < t[j].key }
+
+// cas performs a compare and swap and is inlined into Sort()
+// check with: `go build -gcflags='-m'`.
+func (t tagSet) cas(i, j int) {
+	if t[i].key > t[j].key {
+		t.Swap(i, j)
+	}
+}
+
+// Sort sorts the tagSet in place and is optimized for small (N <= 8) tagSets.
+func (t tagSet) Sort() {
+	// network sort generated with: https://pages.ripco.net/~jgamble/nw.html
+	// using "best": https://metacpan.org/pod/Algorithm::Networksort::Best
+	//
+	// example query (N=6):
+	//   http://jgamble.ripco.net/cgi-bin/nw.cgi?inputs=6&algorithm=best&output=macro
+	//
+	// all cas() methods are inlined, check with: `go build -gcflags='-m'`
+	switch len(t) {
+	case 0, 1:
+		return
+	case 2:
+		t.cas(0, 1)
+	case 3:
+		t.cas(1, 2)
+		t.cas(0, 2)
+		t.cas(0, 1)
+	case 4:
+		t.cas(0, 1)
+		t.cas(2, 3)
+		t.cas(0, 2)
+		t.cas(1, 3)
+		t.cas(1, 2)
+	case 5:
+		t.cas(0, 1)
+		t.cas(3, 4)
+		t.cas(2, 4)
+		t.cas(2, 3)
+		t.cas(0, 3)
+		t.cas(0, 2)
+		t.cas(1, 4)
+		t.cas(1, 3)
+		t.cas(1, 2)
+	case 6:
+		t.cas(1, 2)
+		t.cas(0, 2)
+		t.cas(0, 1)
+		t.cas(4, 5)
+		t.cas(3, 5)
+		t.cas(3, 4)
+		t.cas(0, 3)
+		t.cas(1, 4)
+		t.cas(2, 5)
+		t.cas(2, 4)
+		t.cas(1, 3)
+		t.cas(2, 3)
+	case 7:
+		t.cas(1, 2)
+		t.cas(0, 2)
+		t.cas(0, 1)
+		t.cas(3, 4)
+		t.cas(5, 6)
+		t.cas(3, 5)
+		t.cas(4, 6)
+		t.cas(4, 5)
+		t.cas(0, 4)
+		t.cas(0, 3)
+		t.cas(1, 5)
+		t.cas(2, 6)
+		t.cas(2, 5)
+		t.cas(1, 3)
+		t.cas(2, 4)
+		t.cas(2, 3)
+	case 8:
+		t.cas(0, 1)
+		t.cas(2, 3)
+		t.cas(0, 2)
+		t.cas(1, 3)
+		t.cas(1, 2)
+		t.cas(4, 5)
+		t.cas(6, 7)
+		t.cas(4, 6)
+		t.cas(5, 7)
+		t.cas(5, 6)
+		t.cas(0, 4)
+		t.cas(1, 5)
+		t.cas(1, 4)
+		t.cas(2, 6)
+		t.cas(3, 7)
+		t.cas(3, 6)
+		t.cas(2, 4)
+		t.cas(3, 5)
+		t.cas(3, 4)
+	default:
+		sort.Sort(t)
+	}
+}
+
+// Search is the same as sort.Search() but is optimized for our use case.
+func (t tagSet) Search(key string) int {
+	i, j := 0, len(t)
+	for i < j {
+		h := (i + j) / 2
+		if t[h].key < key {
+			i = h + 1
+		} else {
+			j = h
+		}
+	}
+	return i
+}
+
+// Contains returns if the tagSet contains key.
+func (t tagSet) Contains(key string) bool {
+	if len(t) == 0 {
+		return false
+	}
+	i := t.Search(key)
+	return i < len(t) && t[i].key == key
+}
+
+// Inserts tagPair p into tagSet, if a tagPair with the same key exists it is
+// replaced.
+func (t tagSet) Insert(p tagPair) tagSet {
+	i := t.Search(p.key)
+	if i < len(t) && t[i].key == p.key {
+		t[i].value = p.value
+		return t // exists
+	}
+	// append t to the end of the slice
+	if i == len(t) {
+		return append(t, p)
+	}
+	// insert p
+	t = append(t, tagPair{})
+	copy(t[i+1:], t[i:])
+	t[i] = p
+	return t
+}
+
+// mergeTagSets merges s1 into s2 and stores the result in scratch. Both s1 and
+// s2 must be sorted and s2 can be a sub-slice of scratch if it is located at
+// the tail of the slice (this allows us to allocate only one slice).
+func mergeTagSets(s1, s2, scratch tagSet) tagSet {
+	a := scratch
+	i, j, k := 0, 0, 0
+	for ; i < len(s1) && j < len(s2) && k < len(a); k++ {
+		if s1[i].key == s2[j].key {
+			a[k] = s2[j]
+			i++
+			j++
+		} else if s1[i].key < s2[j].key {
+			a[k] = s1[i]
+			i++
+		} else {
+			a[k] = s2[j]
+			j++
+		}
+	}
+	if i < len(s1) {
+		k += copy(a[k:], s1[i:])
+	}
+	if j < len(s2) {
+		k += copy(a[k:], s2[j:])
+	}
+	return a[:k]
+}
+
+func serializeTagSet(name string, set tagSet) string {
+	// NB: the tagSet must be sorted and have clean values
+
+	const prefix = ".__"
+	const sep = "="
+
+	if len(set) == 0 {
+		return name
+	}
+
+	n := (len(prefix)+len(sep))*len(set) + len(name)
+	for _, p := range set {
+		n += len(p.key) + len(p.value)
+	}
+
+	// CEV: this is same as strings.Builder, but is faster and simpler.
+	b := make([]byte, 0, n)
+	b = append(b, name...)
+	for _, p := range set {
+		b = append(b, prefix...)
+		b = append(b, p.key...)
+		b = append(b, sep...)
+		b = append(b, p.value...)
+	}
+	return *(*string)(unsafe.Pointer(&b))
+}
 
 func serializeTags(name string, tags map[string]string) string {
 	const prefix = ".__"
@@ -41,11 +235,11 @@ func serializeTags(name string, tags map[string]string) string {
 			t1 = t0
 			t0 = tagPair{k, replaceChars(v)}
 		}
-		if t0.dimension > t1.dimension {
+		if t0.key > t1.key {
 			t0, t1 = t1, t0
 		}
-		return name + prefix + t0.dimension + sep + t0.value +
-			prefix + t1.dimension + sep + t1.value
+		return name + prefix + t0.key + sep + t0.value +
+			prefix + t1.key + sep + t1.value
 	case 3:
 		var t0, t1, t2 tagPair
 		for k, v := range tags {
@@ -53,18 +247,18 @@ func serializeTags(name string, tags map[string]string) string {
 			t1 = t0
 			t0 = tagPair{k, replaceChars(v)}
 		}
-		if t1.dimension > t2.dimension {
+		if t1.key > t2.key {
 			t1, t2 = t2, t1
 		}
-		if t0.dimension > t2.dimension {
+		if t0.key > t2.key {
 			t0, t2 = t2, t0
 		}
-		if t0.dimension > t1.dimension {
+		if t0.key > t1.key {
 			t0, t1 = t1, t0
 		}
-		return name + prefix + t0.dimension + sep + t0.value +
-			prefix + t1.dimension + sep + t1.value +
-			prefix + t2.dimension + sep + t2.value
+		return name + prefix + t0.key + sep + t0.value +
+			prefix + t1.key + sep + t1.value +
+			prefix + t2.key + sep + t2.value
 	case 4:
 		var t0, t1, t2, t3 tagPair
 		for k, v := range tags {
@@ -73,25 +267,25 @@ func serializeTags(name string, tags map[string]string) string {
 			t1 = t0
 			t0 = tagPair{k, replaceChars(v)}
 		}
-		if t0.dimension > t1.dimension {
+		if t0.key > t1.key {
 			t0, t1 = t1, t0
 		}
-		if t2.dimension > t3.dimension {
+		if t2.key > t3.key {
 			t2, t3 = t3, t2
 		}
-		if t0.dimension > t2.dimension {
+		if t0.key > t2.key {
 			t0, t2 = t2, t0
 		}
-		if t1.dimension > t3.dimension {
+		if t1.key > t3.key {
 			t1, t3 = t3, t1
 		}
-		if t1.dimension > t2.dimension {
+		if t1.key > t2.key {
 			t1, t2 = t2, t1
 		}
-		return name + prefix + t0.dimension + sep + t0.value +
-			prefix + t1.dimension + sep + t1.value +
-			prefix + t2.dimension + sep + t2.value +
-			prefix + t3.dimension + sep + t3.value
+		return name + prefix + t0.key + sep + t0.value +
+			prefix + t1.key + sep + t1.value +
+			prefix + t2.key + sep + t2.value +
+			prefix + t3.key + sep + t3.value
 	default:
 		// n stores the length of the serialized name + tags
 		n := (len(prefix) + len(sep)) * len(tags)
@@ -101,8 +295,8 @@ func serializeTags(name string, tags map[string]string) string {
 		for k, v := range tags {
 			n += len(k) + len(v)
 			pairs = append(pairs, tagPair{
-				dimension: k,
-				value:     replaceChars(v),
+				key:   k,
+				value: replaceChars(v),
 			})
 		}
 		sort.Sort(pairs)
@@ -112,7 +306,7 @@ func serializeTags(name string, tags map[string]string) string {
 		b = append(b, name...)
 		for _, tag := range pairs {
 			b = append(b, prefix...)
-			b = append(b, tag.dimension...)
+			b = append(b, tag.key...)
 			b = append(b, sep...)
 			b = append(b, tag.value...)
 		}
@@ -134,5 +328,5 @@ func replaceChars(s string) string {
 	if buf == nil {
 		return s
 	}
-	return string(buf)
+	return *(*string)(unsafe.Pointer(&buf))
 }
--- tags_test.go
+++ tags_test.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"math/rand"
 	"sort"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -28,8 +29,8 @@ func serializeTagsReference(name string, tags map[string]string) string {
 
 	buf := new(bytes.Buffer)
 	for _, tag := range tagPairs {
-		if tag.dimension != "" && tag.value != "" {
-			fmt.Fprint(buf, prefix, tag.dimension, sep, tag.value)
+		if tag.key != "" && tag.value != "" {
+			fmt.Fprint(buf, prefix, tag.key, sep, tag.value)
 		}
 	}
 	return name + buf.String()
@@ -166,6 +167,150 @@ func TestSerializeTagDiscardEmptyTagKeyValue(t *testing.T) {
 	}
 }
 
+func TestTagSort(t *testing.T) {
+	contains := func(key string, tags tagSet) bool {
+		for _, t := range tags {
+			if t.key == key {
+				return true
+			}
+		}
+		return false
+	}
+
+	for n := 0; n < 20; n++ {
+		tags := randomTagSet(t, "v", n)
+		keys := make([]string, 0, len(tags)+5)
+		for _, t := range tags {
+			keys = append(keys, t.key)
+		}
+		for i := 0; i < 5; i++ {
+			for {
+				s := RandomString(t, 10)
+				if !contains(s, tags) {
+					keys = append(keys, RandomString(t, 10))
+					break
+				}
+			}
+		}
+		for _, key := range keys {
+			i := tags.Search(key)
+			j := sort.Search(len(tags), func(i int) bool {
+				return tags[i].key >= key
+			})
+			if i != j {
+				t.Errorf("%d: Search got: %d want: %d", n, i, j)
+			}
+		}
+
+		for _, key := range keys {
+			exp := contains(key, tags)
+			got := tags.Contains(key)
+			if exp != got {
+				t.Errorf("%d: tags contains (%q) want: %t got: %t", n, key, exp, got)
+			}
+		}
+
+		for i := range tags {
+			j := tags.Search(tags[i].key)
+			if j != i {
+				t.Errorf("%d: search did not find %q-%d: %d", n, tags[i].key, i, j)
+			}
+		}
+	}
+}
+
+func randomTagSet(t testing.TB, valPrefix string, size int) tagSet {
+	s := make(tagSet, size)
+	for i := 0; i < len(s); i++ {
+		s[i] = tagPair{
+			key:   RandomString(t, 32),
+			value: fmt.Sprintf("%s%d", valPrefix, i),
+		}
+	}
+	s.Sort()
+	return s
+}
+
+func TestTagInsert(t *testing.T) {
+	t1 := randomTagSet(t, "t1_", 1000)
+	t2 := randomTagSet(t, "t2_", 1000)
+	if !sort.IsSorted(t1) {
+		t.Fatal("tags being inserted into must be sorted!")
+	}
+	for i := range t2 {
+		t1 = t1.Insert(t2[i])
+		if !sort.IsSorted(t1) {
+			t.Fatalf("%d: inserting tag failed: %+v", i, t2[i])
+		}
+	}
+}
+
+func mergeTagSetsReference(s1, s2 tagSet) tagSet {
+	seen := make(map[string]bool)
+	var a tagSet
+	for _, t := range s2 {
+		a = append(a, t)
+		seen[t.key] = true
+	}
+	for _, t := range s1 {
+		if !seen[t.key] {
+			a = append(a, t)
+		}
+	}
+	a.Sort()
+	return a
+}
+
+func makeScratch(s1, s2 tagSet) (tagSet, tagSet) {
+	a := make(tagSet, len(s1)+len(s2))
+	copy(a[len(s1):], s2)
+	return a, a[len(s1):]
+}
+
+func tagSetEqual(s1, s2 tagSet) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+	for i := range s1 {
+		if s1[i] != s2[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func TestMergeTagSets(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		s1 := randomTagSet(t, "s1_", i)
+		s2 := randomTagSet(t, "s2_", i)
+		for i := 0; i < len(s2); i++ {
+			if i&1 == 0 {
+				s2[i] = s1[i]
+			}
+		}
+		s1.Sort()
+		s2.Sort()
+		if !sort.IsSorted(s1) {
+			t.Fatal("s1 not sorted")
+		}
+		if !sort.IsSorted(s2) {
+			t.Fatal("s2 not sorted")
+		}
+
+		expected := mergeTagSetsReference(s1, s2)
+		a, to := makeScratch(s1, s2)
+		got := mergeTagSets(s1, to, a)
+		if !sort.IsSorted(got) {
+			t.Errorf("merging %d tagSets failed: not sorted", i)
+		}
+		if !tagSetEqual(got, expected) {
+			// t.Errorf("merging %d tagSets", i)
+			t.Errorf("merging %d tagSets failed\n# Got:\n%+v\n# Want:\n%+v\n# S1:\n%+v\n# S2:\n%+v\n",
+				i, got, expected, s1, s2)
+		}
+	}
+}
+
 func benchmarkSerializeTags(b *testing.B, n int) {
 	const name = "prefix"
 	tags := make(map[string]string, n)
@@ -185,5 +330,198 @@ func BenchmarkSerializeTags(b *testing.B) {
 		b.Run(fmt.Sprintf("%d", i), func(b *testing.B) {
 			benchmarkSerializeTags(b, i)
 		})
+	}
+}
+
+func benchmarkSerializeTagSet(b *testing.B, n int) {
+	const name = "prefix"
+	tags := make(tagSet, 0, n)
+	for i := 0; i < n; i++ {
+		k := fmt.Sprintf("key%d", i)
+		v := fmt.Sprintf("val%d", i)
+		tags = append(tags, tagPair{
+			key:   k,
+			value: v,
+		})
+	}
+	tags.Sort()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		serializeTagSet(name, tags)
+	}
+}
+
+func BenchmarkSerializeTagSet(b *testing.B) {
+	for i := 1; i <= 10; i++ {
+		b.Run(fmt.Sprintf("%d", i), func(b *testing.B) {
+			benchmarkSerializeTagSet(b, i)
+		})
+	}
+}
+
+// TODO (CEV): consider removing this
+func BenchmarkTagSearch(b *testing.B) {
+	rr := rand.New(rand.NewSource(12345))
+	tags := make(tagSet, 5)
+	for i := range tags {
+		tags[i].key = strconv.FormatInt(rr.Int63(), 10)
+		tags[i].value = strconv.FormatInt(rr.Int63(), 10)
+	}
+	tags.Sort()
+
+	for i := 0; i < b.N; i++ {
+		for _, tag := range tags {
+			_ = tags.Search(tag.key)
+		}
+	}
+}
+
+// TODO (CEV): consider removing this
+func BenchmarkTagSearch_Reference(b *testing.B) {
+	rr := rand.New(rand.NewSource(12345))
+	tags := make(tagSet, 5)
+	for i := range tags {
+		tags[i].key = strconv.FormatInt(rr.Int63(), 10)
+		tags[i].value = strconv.FormatInt(rr.Int63(), 10)
+	}
+	tags.Sort()
+
+	for i := 0; i < b.N; i++ {
+		for _, tag := range tags {
+			_ = sort.Search(len(tags), func(i int) bool {
+				return tags[i].key >= tag.key
+			})
+		}
+	}
+}
+
+func benchTagSort(b *testing.B, size int) {
+	// CEV: this isn't super accurate since we also time
+	// the copying the orig slice into the test slice,
+	// but its still useful.
+
+	// use a fixed source so that results are comparable
+	rr := rand.New(rand.NewSource(12345))
+	orig := make(tagSet, size)
+	for i := range orig {
+		orig[i].key = strconv.FormatInt(rr.Int63(), 10)
+		orig[i].value = strconv.FormatInt(rr.Int63(), 10)
+	}
+	rr.Shuffle(len(orig), func(i, j int) {
+		orig[i], orig[j] = orig[j], orig[i]
+	})
+	tags := make(tagSet, len(orig))
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		copy(tags, orig)
+		tags.Sort()
+	}
+}
+
+func BenchmarkTagSort(b *testing.B) {
+	if testing.Short() {
+		b.Skip("short test")
+	}
+	for i := 2; i <= 10; i++ {
+		b.Run(fmt.Sprint(i), func(b *testing.B) {
+			benchTagSort(b, i)
+		})
+	}
+}
+
+// TODO: consider making this benchmark smaller
+func BenchmarkMergeTagSets(b *testing.B) {
+	if testing.Short() {
+		b.Skip("short test")
+	}
+	t1 := make(tagSet, 10)
+	t2 := make(tagSet, 10)
+	for i := 0; i < len(t1); i++ {
+		t1[i] = tagPair{
+			key:   fmt.Sprintf("k1%d", i),
+			value: fmt.Sprintf("v1_%d", i),
+		}
+		t2[i] = tagPair{
+			key:   fmt.Sprintf("k2%d", i),
+			value: fmt.Sprintf("v2_%d", i),
+		}
+	}
+	t1.Sort()
+	t2.Sort()
+
+	scratch := make(tagSet, len(t1)+len(t2))
+
+	b.ResetTimer()
+	b.Run("KeysNotEqual", func(b *testing.B) {
+		for size := 2; size <= 10; size += 2 {
+			b.Run(fmt.Sprint(size), func(b *testing.B) {
+				s1 := t1[:size]
+				s2 := t2[:size]
+				for i := 0; i < b.N; i++ {
+					mergeTagSets(s1, s2, scratch)
+				}
+			})
+		}
+	})
+
+	b.Run("KeysHalfEqual", func(b *testing.B) {
+		for i := range t2 {
+			if i&1 != 0 {
+				t2[i].key = t1[i].key
+			}
+		}
+		t2.Sort()
+		for size := 2; size <= 10; size += 2 {
+			b.Run(fmt.Sprint(size), func(b *testing.B) {
+				s1 := t1[:size]
+				s2 := t2[:size]
+				for i := 0; i < b.N; i++ {
+					mergeTagSets(s1, s2, scratch)
+				}
+			})
+		}
+	})
+
+	b.Run("KeysEqual", func(b *testing.B) {
+		for i := range t2 {
+			t2[i].key = t1[i].key
+		}
+		for size := 2; size <= 10; size += 2 {
+			b.Run(fmt.Sprint(size), func(b *testing.B) {
+				s1 := t1[:size]
+				s2 := t2[:size]
+				for i := 0; i < b.N; i++ {
+					mergeTagSets(s1, s2, scratch)
+				}
+			})
+		}
+	})
+}
+
+func BenchmarkTagSetSearch_Reference(b *testing.B) {
+	var keys [5]string
+	tags := randomTagSet(b, "v_", len(keys))
+	for i := 0; i < len(tags); i++ {
+		keys[i] = tags[i].key
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		key := keys[i%len(keys)]
+		sort.Search(len(tags), func(i int) bool {
+			return tags[i].key >= key
+		})
+	}
+}
+
+func BenchmarkTagSetSearch(b *testing.B) {
+	var keys [5]string
+	tags := randomTagSet(b, "v_", len(keys))
+	for i := 0; i < len(tags); i++ {
+		keys[i] = tags[i].key
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		tags.Search(keys[i%len(keys)])
 	}
 }
