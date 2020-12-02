1016a6829acc405ab91b6dbce023ee3d68d635de
Khanh Le Do <5936195+khanhldt@users.noreply.github.com>
2020-12-01 23:31:53 -0800

    Update vuln.go

--- vuln.go
+++ vuln.go
@@ -1,18 +1,8 @@
-package main
+package vuln
 import (
-    "database/sql"
     "fmt"
     "os"
 )
 func main(){
-    db, err := sql.Open("sqlite3", ":memory:")
-    if err != nil {
-        panic(err)
-    }
     q := fmt.Sprintf("SELECT * FROM foo where name = '%s'", os.Args[1])
-    rows, err := db.Query(q)
-    if err != nil {
-        panic(err)
-    }
-    defer rows.Close()
 }
